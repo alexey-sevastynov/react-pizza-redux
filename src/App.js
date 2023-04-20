43a215d02b9b9dc3c79f5d217f9feefbc075b9db
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-04-20 21:49:14 +0300

    made fake JSON data PIZZAS

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import Header from "./components/Header/Header";
 import Layout from "./components/Layout";
 import MainPage from "./page/mainPage/MainPage";
 import BasketPage from "./page/basketPage/BasketPage";
+import { useEffect, useState } from "react";
 
 function App() {
   const sorts = ["popularity", "price", "alphabet"];
   const categories = ["Meat", "Vegan", "Grill", "Spicy"];
+
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/db.json").then((response) =>
+      response.json().then((json) => {
+        setPizzas(json.pizzas);
+      })
+    );
+  }, []);
 
   return (
     <>
@@ -16,7 +27,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route
             index
-            element={<MainPage categories={categories} sorts={sorts} />}
+            element={
+              <MainPage categories={categories} sorts={sorts} pizzas={pizzas} />
+            }
           />
           <Route path="basket" element={<BasketPage />} />
         </Route>
