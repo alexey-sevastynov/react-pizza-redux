4cb7d81c42fbed53c4546f900502d4e2bf20ff9d
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-04-20 17:02:27 +0300

    useRef. popup window sort

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,67 +1,71 @@
 import React from "react";
 
-class Categories extends React.Component {
-  categories = ["Meat", "Vegan", "Grill", "Spicy"];
+// class Categories extends React.Component {
+//   categories = ["Meat", "Vegan", "Grill", "Spicy"];
 
-  state = {
-    activeItem: null,
-  };
-
-  onSelectIndex = (index) => {
-    this.setState({ activeItem: index });
-  };
-
-  render() {
-    const { activeItem } = this.state;
-
-    const items = this.categories.map((category, index) => (
-      <li
-        key={category}
-        className={activeItem === index ? "active" : ""}
-        onClick={() => this.onSelectIndex(index)}
-      >
-        {category}
-      </li>
-    ));
-
-    return (
-      <div className="categories">
-        <ul>
-          <li className="active">All</li>
-          {items}
-        </ul>
-      </div>
-    );
-  }
-}
-
-// function Categories({ onClickItem }) {
-//   const categories = ["Meat", "Vegan", "Grill", "Spicy"];
-
-//   const [activeItem, setItemActive] = React.useState(null);
-
-//   const onSelectIndex = (index) => {
-//     setItemActive(index);
+//   state = {
+//     activeItem: null,
 //   };
 
-//   const items = categories.map((category, index) => (
-//     <li
-//       key={category}
-//       className={activeItem === index ? "active" : ""}
-//       onClick={() => onSelectIndex(index)}
-//     >
-//       {category}
-//     </li>
-//   ));
+//   onSelectIndex = (index) => {
+//     this.setState({ activeItem: index });
+//   };
 
-//   return (
-//     <div className="categories">
-//       <ul>
-//         <li className="active">All</li>
-//         {items}
-//       </ul>
-//     </div>
-//   );
+//   render() {
+//     const { activeItem } = this.state;
+
+//     const items = this.categories.map((category, index) => (
+//       <li
+//         key={category}
+//         className={activeItem === index ? "active" : ""}
+//         onClick={() => this.onSelectIndex(index)}
+//       >
+//         {category}
+//       </li>
+//     ));
+
+//     return (
+//       <div className="categories">
+//         <ul>
+//           <li className="active">All</li>
+//           {items}
+//         </ul>
+//       </div>
+//     );
+//   }
 // }
+
+function Categories({ onClickItem }) {
+  const categories = ["Meat", "Vegan", "Grill", "Spicy"];
+
+  const [activeItem, setItemActive] = React.useState(null);
+
+  const onSelectItem = (index) => {
+    setItemActive(index);
+  };
+
+  const items = categories.map((category, index) => (
+    <li
+      key={category}
+      className={activeItem === index ? "active" : ""}
+      onClick={() => onSelectItem(index)}
+    >
+      {category}
+    </li>
+  ));
+
+  const styleLiAll = activeItem === null ? "active" : "";
+
+  return (
+    <div className="categories">
+      <ul>
+        <li className={styleLiAll} onClick={() => onSelectItem(null)}>
+          All
+        </li>
+        {items}
+      </ul>
+    </div>
+  );
+}
 
 export default Categories;
