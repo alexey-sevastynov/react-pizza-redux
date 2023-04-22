01818462906d68fe4d3e89efb43408a4657168be
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-04-22 14:42:19 +0300

    FUNCTION component REDUX

--- src/App.js
+++ src/App.js
@@ -10,101 +10,110 @@ import Layout from "./components/Layout";
 import MainPage from "./page/mainPage/MainPage";
 import BasketPage from "./page/basketPage/BasketPage";
 
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { setPizzas } from "./redux/actions/pizzas";
 
-// function App() {
-//   const sorts = [
-//     { name: "popularity", type: "popular" },
-//     { name: "price", type: "price" },
-//     { name: "alphabet", type: "alphabet" },
-//   ];
-//   const categories = ["Meat", "Vegan", "Grill", "Spicy"];
+function App() {
+  const dispatch = useDispatch();
+  const { pizzas } = useSelector((state) => {
+    return {
+      pizzas: state.pizzas.items,
+    };
+  });
 
-//   useEffect(() => {
-//     //____________AXIOS
-//     axios
-//       .get("http://localhost:3000/db.json")
-//       .then(({ data }) => setPizzas(data.pizzas));
-
-//     //_____________FETCH
-//     // fetch("http://localhost:3000/db.json").then((response) =>
-//     //   response.json().then((json) => {
-//     //     setPizzas(json.pizzas);
-//     //   })
-//     // );
-//   }, []);
-
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route
-//             index
-//             element={
-//               <MainPage categories={categories} sorts={sorts} pizzas={pizzas} />
-//             }
-//           />
-//           <Route path="basket" element={<BasketPage />} />
-//         </Route>
-//       </Routes>
-//     </>
-//   );
-// }
-
-class App extends React.Component {
-  state = {
-    pizzas: [],
-  };
-
-  sorts = [
+  const sorts = [
     { name: "popularity", type: "popular" },
     { name: "price", type: "price" },
     { name: "alphabet", type: "alphabet" },
   ];
-  categories = ["Meat", "Vegan", "Grill", "Spicy"];
+  const categories = ["Meat", "Vegan", "Grill", "Spicy"];
 
-  componentDidMount() {
+  useEffect(() => {
+    //____________AXIOS
     axios
       .get("http://localhost:3000/db.json")
-      .then(({ data }) => this.props.showPizzas(data.pizzas));
-  }
+      .then(({ data }) => dispatch(setPizzas(data.pizzas)));
 
-  render() {
-    return (
-      <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route
-              index
-              element={
-                <MainPage
-                  categories={this.categories}
-                  sorts={this.sorts}
-                  pizzas={this.props.items}
-                />
-              }
-            />
-            <Route path="basket" element={<BasketPage />} />
-          </Route>
-        </Routes>
-      </>
-    );
-  }
+    //_____________FETCH
+    // fetch("http://localhost:3000/db.json").then((response) =>
+    //   response.json().then((json) => {
+    //     setPizzas(json.pizzas);
+    //   })
+    // );
+  }, []);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <MainPage categories={categories} sorts={sorts} pizzas={pizzas} />
+            }
+          />
+          <Route path="basket" element={<BasketPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-    filters: state.filters,
-  };
-};
+export default App;
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    //showPizzas called in componentDidMount()!!!
-    showPizzas: (items) => dispatch(setPizzas(items)),
-  };
-};
+// class App extends React.Component {
+//   state = {
+//     pizzas: [],
+//   };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+//   sorts = [
+//     { name: "popularity", type: "popular" },
+//     { name: "price", type: "price" },
+//     { name: "alphabet", type: "alphabet" },
+//   ];
+//   categories = ["Meat", "Vegan", "Grill", "Spicy"];
+
+//   componentDidMount() {
+//     axios
+//       .get("http://localhost:3000/db.json")
+//       .then(({ data }) => this.props.showPizzas(data.pizzas));
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route
+//               index
+//               element={
+//                 <MainPage
+//                   categories={this.categories}
+//                   sorts={this.sorts}
+//                   pizzas={this.props.items}
+//                 />
+//               }
+//             />
+//             <Route path="basket" element={<BasketPage />} />
+//           </Route>
+//         </Routes>
+//       </>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.pizzas.items,
+//     filters: state.filters,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     //showPizzas called in componentDidMount()!!!
+//     showPizzas: (items) => dispatch(setPizzas(items)),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
